320a2ef2245cd732406f945b0236d646f272d3b5
Lucas Pluvinage <lucas@tarides.com>
2021-09-22 13:46:46 +0200

    current-deployer-daemon: save state

--- lib/current-deployer-daemon/main.ml
+++ lib/current-deployer-daemon/main.ml
@@ -1,3 +1,9 @@
+open Lwt.Syntax
+
+let socket_path = "/var/run/current-deployer/current-deployerd.sock"
+
+let db_path = "/var/lib/current-deployer/db"
+
 module Config = struct
   type t = { network : Ipaddr.V4.Prefix.t }
 end
@@ -103,6 +109,19 @@ module State = struct
         Ok removed_deployment
     | None -> Error `Not_found
 
+  let nat_redirection ~to_ip ~source ~target =
+    let module Nat = Iptables.Nat in
+    let match_tcp =
+      Nat.Match.(
+        protocol (`Tcp (Tcp.v ~dport:(Nat.Negatable.V [ Port source ]) ())))
+    in
+    let match_udp =
+      Nat.Match.(
+        protocol (`Udp (Udp.v ~dport:(Nat.Negatable.V [ Port source ]) ())))
+    in
+    let action = Nat.Action.destination_nat ~port:(Nat.Port target) to_ip in
+    [ Nat.Rule.v [ match_tcp ] action; Nat.Rule.v [ match_udp ] action ]
+
   let add_new_deployment t (deployment : Types.DeploymentInfo.t) =
     let existing_deployment =
       List.exists
@@ -126,22 +145,7 @@ module State = struct
         List.map
           (fun { Types.PortRedirection.source; target } ->
             if PortSet.mem source ports then raise (Port_conflict source);
-            let module Nat = Iptables.Nat in
-            let match_tcp =
-              Nat.Match.(
-                protocol
-                  (`Tcp (Tcp.v ~dport:(Nat.Negatable.V [ Port source ]) ())))
-            in
-            let match_udp =
-              Nat.Match.(
-                protocol
-                  (`Udp (Udp.v ~dport:(Nat.Negatable.V [ Port source ]) ())))
-            in
-            let action =
-              Nat.Action.destination_nat ~port:(Nat.Port target)
-                deployment.ip.ip
-            in
-            [ Nat.Rule.v [ match_tcp ] action; Nat.Rule.v [ match_udp ] action ])
+            nat_redirection ~to_ip:deployment.ip.ip ~source ~target)
           deployment.ports
         |> List.concat
       in
@@ -149,6 +153,45 @@ module State = struct
       t.deployments <- (handles, deployment) :: t.deployments;
       Ok ()
     with Port_conflict v -> Error (`Port_already_allocated v)
+
+  module Db = struct
+    type state = t
+
+    type t = { ips : string Ipmap.t; deployments : Types.DeploymentInfo.t list }
+
+    let of_file () : t Lwt.t =
+      Lwt_io.with_file ~mode:Input db_path (fun channel ->
+          Lwt_io.read_value channel)
+
+    let mutex = Lwt_mutex.create ()
+
+    let to_file t =
+      let _ =
+        Bos.OS.Dir.create ~mode:0o700
+          (Fpath.of_string db_path |> Result.get_ok |> Fpath.parent)
+      in
+      Lwt_mutex.with_lock mutex @@ fun () ->
+      Lwt_io.with_file ~mode:Output db_path (fun channel ->
+          Lwt_io.write_value channel t)
+
+    let of_state (t : state) =
+      { ips = t.ips; deployments = t.deployments |> List.map snd }
+
+    let to_state (t : t) =
+      let state = v () in
+      state.ips <- t.ips;
+      List.iter
+        (fun x -> add_new_deployment state x |> Result.get_ok)
+        t.deployments;
+      state
+  end
+
+  let v () =
+    Lwt.catch
+      (fun () ->
+        let+ v = Db.of_file () in
+        Db.to_state v)
+      (function exn -> Lwt.fail exn)
 end
 
 let safe_close fd =
@@ -208,16 +251,18 @@ module Wire = struct
 
   let create_socket =
     let open Lwt.Infix in
-    let name = "/var/run/current-deployer/current-deployerd.sock" in
-    (Lwt_unix.file_exists name >>= function
-     | true -> Lwt_unix.unlink name
-     | false -> Bos.OS.Dir.create (Fpath.of_string name |> Result.get_ok) |> Result.get_ok |> ignore; Lwt.return_unit)
+    (Lwt_unix.file_exists socket_path >>= function
+     | true -> Lwt_unix.unlink socket_path
+     | false ->
+         Bos.OS.Dir.create (Fpath.of_string socket_path |> Result.get_ok)
+         |> Result.get_ok |> ignore;
+         Lwt.return_unit)
     >>= fun () ->
     let s = Lwt_unix.(socket PF_UNIX SOCK_STREAM 0) in
     Lwt_unix.set_close_on_exec s;
     let old_umask = Unix.umask 0 in
-    Lwt_unix.(bind s (ADDR_UNIX name)) >|= fun () ->
-    Logs.app (fun m -> m "listening on %s" name);
+    Lwt_unix.(bind s (ADDR_UNIX socket_path)) >|= fun () ->
+    Logs.app (fun m -> m "listening on %s" socket_path);
     let _ = Unix.umask old_umask in
     Lwt_unix.listen s 1;
     s
@@ -280,7 +325,7 @@ let handle ~handlers socket =
 let handlers ~state =
   let module Spec = Current_deployer_api.Spec in
   Handler.
-    [ 
+    [
       (* IPs *)
       implement Spec.IpManager.list (fun () -> State.list_ips state);
       implement Spec.IpManager.request (fun (tag, prefix, blacklist) ->
@@ -298,13 +343,15 @@ let handlers ~state =
   |> Handler.Rpc.Tag.Map.of_seq
 
 let job =
-  let state = State.v () in
+  let* state = State.v () in
   let handlers = handlers ~state in
   let open Lwt.Syntax in
   let* socket = Wire.create_socket in
   let rec loop () =
     let* client_socket, _ = Lwt_unix.accept socket in
-    Lwt.async (fun () -> handle ~handlers client_socket);
+    Lwt.async (fun () ->
+        let* () = handle ~handlers client_socket in
+        State.Db.of_state state |> State.Db.to_file);
     loop ()
   in
   loop ()
--- lib/current-deployer-daemon/packaging/Linux/install.sh
+++ lib/current-deployer-daemon/packaging/Linux/install.sh
@@ -3,6 +3,7 @@ CURRENT_DEPLOYER_USER=current-deployer
 
 groupadd -g 497 $CURRENT_DEPLOYER_USER
 useradd -g 497 -u 497 -d /nonexistent -s /usr/sbin/nologin $CURRENT_DEPLOYER_USER
+sudo mkdir -m 0700 -p /var/lib/current-deployer/
 
 sudo cp ../../_build/install/default/bin/current-deployerd /usr/local/sbin/
 sudo cp ./current_deployerd.service /etc/systemd/system/
